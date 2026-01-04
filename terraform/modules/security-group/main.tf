8738bd4cc4949f35dfc0602c06ef832b7c7ae88a
DongQuach1804 <22520259@gm.uit.edu.vn>
2026-01-05 06:51:26 +0700

    Add security groups for public and private EC2

---
+++ terraform/modules/security-group/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "public_ec2" {
+  name   = "lab01-public-ec2-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lab01-public-ec2-sg"
+  }
+}
+
+resource "aws_security_group" "private_ec2" {
+  name   = "lab01-private-ec2-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description     = "SSH from public EC2"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_ec2.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lab01-private-ec2-sg"
+  }
+}
---
+++ terraform/modules/security-group/variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "my_ip" {
+  description = "Your public IP for SSH access (x.x.x.x/32)"
+  type        = string
+}
