9adc734a38a1cef8686f5432389b3c2ebd13a324
DongQuach1804 <22520259@gm.uit.edu.vn>
2026-01-05 07:00:44 +0700

    Finalize root terraform wiring and variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,44 @@ module "vpc" {
 }
 
 module "subnet" {
-  source               = "./modules/subnet"
-  vpc_id               = module.vpc.vpc_id
-  public_subnet_cidr   = var.public_subnet_cidr
-  private_subnet_cidr  = var.private_subnet_cidr
+  source              = "./modules/subnet"
+  vpc_id              = module.vpc.vpc_id
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+}
+
+module "igw" {
+  source = "./modules/igw"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "nat" {
+  source            = "./modules/nat"
+  public_subnet_id  = module.subnet.public_subnet_id
+}
+
+module "route_table" {
+  source            = "./modules/route-table"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_id  = module.subnet.public_subnet_id
+  private_subnet_id = module.subnet.private_subnet_id
+  igw_id            = module.igw.igw_id
+  nat_gateway_id    = module.nat.nat_gateway_id
+}
+
+module "security_group" {
+  source = "./modules/security-group"
+  vpc_id = module.vpc.vpc_id
+  my_ip  = var.my_ip
+}
+
+module "ec2" {
+  source            = "./modules/ec2"
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  public_subnet_id  = module.subnet.public_subnet_id
+  private_subnet_id = module.subnet.private_subnet_id
+  public_sg_id      = module.security_group.public_ec2_sg_id
+  private_sg_id     = module.security_group.private_ec2_sg_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,23 @@ variable "private_subnet_cidr" {
   type    = string
   default = "10.0.2.0/24"
 }
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0df7a207adb9748c7"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your public IP x.x.x.x/32"
+  type        = string
+}
